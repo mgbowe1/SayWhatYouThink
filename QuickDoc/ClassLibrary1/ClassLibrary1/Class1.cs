33f6f47b7e958e5d2e4d9c345e486543b08787ac
mgbowe1 <michael.g.bowe@gmail.com>
2016-10-22 08:50:49 -0400

    Add VS Project

---
+++ QuickDoc/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.SpeechRecognition;
+
+namespace STT
+{
+    public class SpeechRec
+    {
+        private string _api_key;
+        private DataRecognitionClient _data_client;
+        private MicrophoneRecognitionClient _mic_client;
+        private SpeechRecognitionMode _mode;
+
+        public SpeechRec(string api_key)
+        {
+            _api_key = api_key;
+            _mode = SpeechRecognitionMode.ShortPhrase;
+        }
+
+        private void _mic_status_handler(object sender, MicrophoneEventArgs e)
+        {
+            if (e.Recording)
+            {
+                Console.WriteLine("Please start speaking");
+            }
+        }
+
+        private void _partial_result_handler(object sender, PartialSpeechResponseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _full_result_handler(object sender, SpeechResponseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _conversion_error_handler(object sender, SpeechErrorEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _create_mic_client()
+        {
+            _mic_client = SpeechRecognitionServiceFactory.CreateMicrophoneClient(_mode, "en-US", _api_key);
+
+            _mic_client.OnMicrophoneStatus += _mic_status_handler;
+            _mic_client.OnPartialResponseReceived += _partial_result_handler;
+            _mic_client.OnResponseReceived += _full_result_handler;
+            _mic_client.OnConversationError += _conversion_error_handler;
+        }
+
+        public void StartSpeechRec()
+        {
+            this._create_mic_client();
+            _mic_client.StartMicAndRecognition();
+        }
+    }
+}
---
+++ QuickDoc/QuickDoc/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace QuickDoc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private Key[] command = new Key[2];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.KeyDown += MainWindow_KeyDown;
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.LeftAlt:
+                case Key.RightAlt:
+                    if (command.Length == 0)
+                        command[0] = e.Key;
+                    break;
+                case Key.Q:
+                    if (command.Length == 1)
+                        command[1] = e.Key;
+                    break;
+            }
+
+            if (command.Length == 2)
+            {
+                Process[] processes = Process.GetProcessesByName("notepad");
+                if (processes.Length > 0)
+                {
+                    Process p = processes[0];
+                    IntPtr ptr = p.Handle;
+                    SetForegroundWindow(ptr);
+                    System.Windows.Forms.SendKeys.SendWait("Hello World!");
+                }
+
+                Array.Clear(command, 0, 2);
+            }
+        }
+    }
+}
