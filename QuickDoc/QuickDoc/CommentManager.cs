f470069d6bf9ab70e763c5cfc1185c9afc8cf324
purvakamat <purvakamat@ccs.neu.edu>
2016-10-22 15:23:29 -0400

    Added document library
    CommentManager functionality needs to be moved to DocumentLibrary.

---
+++ QuickDoc/QuickDoc/CommentManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+
+namespace QuickDoc
+{
+    public class CommentManager
+    {
+        private static CommentManager _instance = null;
+
+        private CommentManager() { }
+
+        public static CommentManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new CommentManager();
+                return _instance;
+            }
+        }
+
+        public void WriteComment(string comment)
+        {
+            SendKeys.Send(comment);
+        }
+    }
+}
---
+++ QuickDoc/QuickDoc/VoiceCommands.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickDoc
+{
+    public class VoiceCommands
+    {
+        const string START_COMMENT = "Start comment";
+        const string END_COMMENT = "End comment";
+    }
+}
