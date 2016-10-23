dfcbf78c60ff615bd29669008499c36216433ebe
purvakamat <purvakamat@ccs.neu.edu>
2016-10-22 20:31:21 -0400

    Check-in remnants for console app

---
+++ QuickDoc/QuickDocConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Documentation;
+
+namespace QuickDocConsole
+{
+    class Program
+    {
+        Documentation.Documentation doc = null;
+        public Boolean waitForMessage = true;
+
+        public Program(string lang)
+        {
+            doc = GetDocumentationType(lang);
+
+            SpeechRecManager.Instance.StartListeningForComment();
+            SpeechRecManager.Instance.OnSpeechReceived += SpeechRecManager_OnSpeechReceived;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                Program p = new Program(args[0]);
+                while (p.waitForMessage) { }
+                Environment.Exit(0);
+            }
+        }
+
+        private void SpeechRecManager_OnSpeechReceived(object sender, EventArgs e)
+        {
+            doc.Message = SpeechRecManager.Instance.GetResponse();
+            string response = doc.getDocumentedString();
+            Console.WriteLine(response);
+            //Console.ReadLine();
+            waitForMessage = false;
+        }
+
+        private Documentation.Documentation GetDocumentationType(string lang)
+        {
+            Documentation.Documentation doc = null;
+            switch (lang)
+            {
+                case "P":
+                    doc = new PythonDocumentation();
+                    break;
+                case "L":
+                    doc = new RacketDocumentation();
+                    break;
+                case "C":
+                    doc = new CDocumentation();
+                    break;
+            }
+            return doc;
+        }
+    }
+}
---
+++ QuickDoc/QuickDocConsole/SpeechRecManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using SpeechRecLib;
+
+
+namespace QuickDocConsole
+{
+    public class SpeechRecManager
+    {
+        private static SpeechRecManager _instance = null;
+        private SpeechRec _speechRec;
+        private string _response = "";
+
+        public event EventHandler OnSpeechReceived;
+
+        private SpeechRecManager()
+        {
+            _speechRec = new SpeechRec("83d360c791ae43e795500e1ae43fcd6c");
+            _speechRec.OnSpeechReceived += SpeechRec_OnSpeechReceived;
+        }
+
+        public static SpeechRecManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SpeechRecManager();
+                }
+                return _instance;
+            }
+        }
+
+        public void StartListeningForComment()
+        {
+            _speechRec.StartSpeechRec();
+        }
+
+        private void SpeechRec_OnSpeechReceived(object sender, SpeechReceivedEventArgs e)
+        {
+            if (e.Success)
+            {
+                _response = e.Result;
+                OnSpeechReceived?.Invoke(this, new EventArgs());
+            }
+        }
+
+        public string GetResponse()
+        {
+            return _response;
+        }
+
+        public void StopListeningForComment()
+        {
+            _speechRec.StopSpeechRec();
+        }
+    }
+}
